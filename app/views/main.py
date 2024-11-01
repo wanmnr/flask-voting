2f7f4b66c8be913ede388d172f578f426a69e49d
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-01 19:01:03 +0800

    feat(core): Add new route
    Add routes to template:
    templates/index.html
    templates/about.html

--- app/views/main.py
+++ app/views/main.py
@@ -1,9 +1,17 @@
 # app/views/main.py
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
+def home():
+    return render_template('index.html')
+
+@main_bp.route('/hello')
 def hello():
     return 'Hello World!'
+
+@main_bp.route('/about')
+def about():
+    return render_template('about.html')
