b77fa861f08bf7a63535793d58b4d1050dd21ff1
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-02 14:33:50 +0800

    chore(run.py): Load environment settings and set up current environment

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
 # run.py
 
 from app import create_app
+from config import DevelopmentConfig, ProductionConfig
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+# Use environment variable to switch between configurations
+env = os.environ.get('FLASK_ENV', 'development')
+config = DevelopmentConfig if env == 'development' else ProductionConfig
 
 app = create_app()
+app.config.from_object(config)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
