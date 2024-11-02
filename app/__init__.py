93e813f43094c1f411bde390c6ebfb32fe050356
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-02 14:13:48 +0800

    style(__init__): Configure flask-assets and cssmin

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,27 @@
 # app/__init__.py
 
 from flask import Flask
+from flask_assets import Environment, Bundle
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    # Initialize Flask-Assets
+    assets = Environment(app)
+    
+    # Define asset bundles
+    css = Bundle(
+        'css/style.css',
+        filters='cssmin',
+        output='gen/packed.css'
+    )
+    # Register bundles
+    assets.register('css_all', css)
+
     # Register blueprints here if needed
     # Example: from .views.main import main
     # Register blueprints
