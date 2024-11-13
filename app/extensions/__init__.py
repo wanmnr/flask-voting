df0752c802aa34696e7eb5cc2ceb87804ea789b3
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-13 14:20:26 +0800

    refactor(extensions): update extension imports and add `Migrate` instance
    - Removed redundant `init_assets` import as assets initialization is now handled within the `FlaskAssets` class
    - Added `Migrate` instance for database migrations support
    - Updated `__all__` to reflect modified imports

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -1,15 +1,19 @@
 # app/extensions/__init__.py
-from app.extensions.db import db, init_db
+from app.extensions.db import db
 from app.extensions.login import login_manager, init_login_manager
-from app.extensions.assets import assets, init_assets
+from app.extensions.assets import assets
 from app.extensions.cache import cache, init_cache
 from app.extensions.mail import mail, init_mail
 from app.extensions.limiter import limiter
 
+from flask_migrate import Migrate
+
+migrate = Migrate()
+
 __all__ = [
-    'db', 'init_db',
+    'db',
     'login_manager', 'init_login_manager',
-    'assets', 'init_assets',
+    'assets',
     'cache', 'init_cache',
     'mail', 'init_mail',
     'limiter'
