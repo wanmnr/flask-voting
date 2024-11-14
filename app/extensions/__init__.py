37361407b1ea73584ba32c60921ae52bc5a19e6c
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-14 22:41:47 +0800

    refactor(extension): update extensions to include new modules and initialization functions
    - Updated `app/extensions/__init__.py` to include `admin`, `adminlte3`, `babel`, `jwt`, `principal`, and `security` extensions.
    - Added initialization functions for `admin`, `jwt`, and `security` for better modular setup.
    - Included `Marshmallow` and `Migrate` for data serialization and database migration.

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -1,20 +1,34 @@
 # app/extensions/__init__.py
-from app.extensions.db import db
-from app.extensions.login import login_manager, init_login_manager
+from app.extensions.admin import admin, init_admin
+from app.extensions.adminlte3 import adminlte3
 from app.extensions.assets import assets
+from app.extensions.babel import babel
 from app.extensions.cache import cache, init_cache
-from app.extensions.mail import mail, init_mail
+from app.extensions.db import db
+from app.extensions.jwt import jwt, init_jwt
 from app.extensions.limiter import limiter
+from app.extensions.login import login_manager
+from app.extensions.mail import mail, init_mail
+from app.extensions.principal import principal
+from app.extensions.security import security, init_security
 
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 
+marshmallow = Marshmallow()
 migrate = Migrate()
 
 __all__ = [
-    'db',
-    'login_manager', 'init_login_manager',
+    'admin', 'init_admin',
+    'adminlte3',
     'assets',
+    'babel',
     'cache', 'init_cache',
+    'db',
+    'jwt', 'init_jwt',
+    'limiter',
+    'login_manager', 'init_login_manager',
     'mail', 'init_mail',
-    'limiter'
+    'principal',
+    'security', 'init_security',
 ]
