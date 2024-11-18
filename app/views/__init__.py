575953d050ec48d5d422aedcc86349baeec147af
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-18 23:33:01 +0800

    refactor(views): update blueprint registeration in views/__init__.py with new 'users' and 'mock_error' views
    - Added 'users_bp' for user-related routes.
    - Added 'mock_bp' for mock error routes.
    - Renamed 'admin_bp' to 'myadmin_bp' for better clarity.
    - Removed 'api_bp' as it's no longer needed.

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,14 +2,16 @@
 from flask import Blueprint
 from app.views.main import main_bp
 from app.views.auth import auth_bp
-from app.views.admin import admin_bp
-from app.views.api import api_bp
+from app.views.admin import myadmin_bp
 from app.views.polls import polls_bp
+from app.views.users import users_bp
+from app.views.mock_error import mock_bp
 
 def register_blueprints(app):
     """Register Flask blueprints."""
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp, url_prefix='/auth')
-    app.register_blueprint(admin_bp, url_prefix='/admin')
-    app.register_blueprint(api_bp, url_prefix='/api')
+    app.register_blueprint(myadmin_bp, url_prefix='/admin')
     app.register_blueprint(polls_bp, url_prefix='/polls')
+    app.register_blueprint(users_bp, url_prefix='/users')
+    app.register_blueprint(mock_bp, url_prefix='/mock')
