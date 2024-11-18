43b13bd0168dc460823b2faa2d86960f36f8f4e2
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-18 23:04:05 +0800

    refactor(login): add `User` import inside the `load_user` function
    - Added `User` import statement inside the `load_user` function to avoid circular imports and improve code organization.

--- app/extensions/login.py
+++ app/extensions/login.py
@@ -2,10 +2,13 @@
 
 from flask_login import LoginManager
 
+
+
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
 
 @login_manager.user_loader
 def load_user(user_id):
+    from app.models.user import User
     return User.query.get(int(user_id))
